5d71c974756edb48d5d15a860ab12bcca6886f70
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2018-09-17 23:03:14 -0400

    Add project files.

---
+++ OptionsCalc/Form2.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace OptionsCalc
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.RowTemplate.Height = 37;
+            this.dataGridView1.Size = new System.Drawing.Size(1987, 1158);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellUIChange);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1987, 1158);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ OptionsCalc/Form2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptionsCalc
+{
+    public partial class Form2 : Form
+    {
+        double[,] data;
+        DateTime today;
+
+        private void PopulateDataGrid(double[,] myArr)
+        {
+            DataTable dataTable = new DataTable();
+            for (int j = 0; j < myArr.GetLength(1); j++)
+                dataTable.Columns.Add(new DataColumn("Column " + j.ToString()));
+
+            for (int i = 0; i < myArr.GetLength(0); i++)
+            {
+                var newRow = dataTable.NewRow();
+                for (int j = 0; j < myArr.GetLength(1); j++)
+                {
+                    newRow["Column " + j.ToString()] = myArr[i, j];
+                }
+                dataTable.Rows.Add(newRow);
+            }
+            this.dataGridView1.DataSource = dataTable;
+            dataGridView1.Columns[0].Width = 50;
+            dataGridView1.Columns[0].HeaderText = "Price";
+            for (int k = 1; k < data.GetLength(0); k++)
+            {
+                dataGridView1.Rows[k].Height = 50;
+            }
+            for (int k = 1; k < data.GetLength(1); k++)
+            {
+                dataGridView1.Columns[k].Width = 50;
+                dataGridView1.Columns[k].HeaderText = today.AddDays(k-1).ToShortDateString();
+            }
+            dataGridView1.Columns[0].Frozen = true;
+        }
+
+        public Form2(double[,] graph, DateTime today)
+        {
+            InitializeComponent();
+            data = graph;
+            this.today = today;
+            PopulateDataGrid(graph);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellUIChange(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 1; j < data.GetLength(1); j++)
+                {
+
+                    double a = data[i, j];
+                    int x = Convert.ToInt32(data[i, j]);
+                    
+                    if (x > 0)
+                    {
+                        if((100-x)*2 < 0)
+                        {
+                            x = 100;
+                        }
+                        this.dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.FromArgb(255, (100 - x) * 2, 255, (100 - x) * 2);
+                    }
+                    else if (x < 0)
+                    { 
+                        if((100+x)*2 < 0)
+                        {
+                            x = -100;
+                        }
+                        this.dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.FromArgb(255, 255, (100 + x) * 2, (100 + x) * 2);
+                    }
+                }
+            }
+            
+        }
+    }
+}
